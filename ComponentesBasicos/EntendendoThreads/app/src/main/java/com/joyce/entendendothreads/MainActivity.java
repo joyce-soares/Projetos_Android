fd3fd4f465a1f54cbbf025710b7fe82a1a42d48b
Joyce Soares <joycespn6@gmail.com>
2021-10-18 16:31:41 -0300

    entendendo threads

--- ComponentesBasicos/EntendendoThreads/app/src/main/java/com/joyce/entendendothreads/MainActivity.java
+++ ComponentesBasicos/EntendendoThreads/app/src/main/java/com/joyce/entendendothreads/MainActivity.java
@@ -4,25 +4,40 @@ package com.joyce.entendendothreads;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btIniciar;
+    int i;
+    boolean pararExecucao = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btIniciar = findViewById(R.id.button_iniciar);
     }
 
     public void iniciarThread(View view){
+
+        pararExecucao = false;
         /*
+        //executando uma acao em uma thread paralela
         MyThread thread = new MyThread();
         thread.start();*/
 
+        //executando uma acao em uma thread paralela
         MyRunnable runnable = new MyRunnable();
         new Thread( runnable ).start();
 
+
+    }
+
+    public void pararThread(View v){
+        pararExecucao = true;
 
     }
 
@@ -30,9 +45,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            for (int i=0; i <= 15; i++ ){
+            for ( i=0; i <= 15; i++ ){
+
+                if(pararExecucao)
+                    return;
 
                 Log.d("Thread", "contador: " + i );
+
+                //execuntando mudanca de interface apenas na thread principal(UiThread)
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btIniciar.setText("contador " + i);
+                    }
+                });
 
                 try {
                     Thread.sleep(1000);
@@ -48,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            for (int i=0; i <= 15; i++ ){
+            for (i=0; i <= 15; i++ ){
 
                 Log.d("Thread", "contador: " + i );
 
