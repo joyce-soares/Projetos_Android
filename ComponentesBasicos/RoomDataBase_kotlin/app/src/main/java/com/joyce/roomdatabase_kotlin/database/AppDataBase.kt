ec171818ffc35fa85f41e589373dd55db12be894
Joyce Soares <joycespn6@gmail.com>
2021-11-11 13:13:21 -0300

    criando db

--- ComponentesBasicos/RoomDataBase_kotlin/app/src/main/java/com/joyce/roomdatabase_kotlin/database/AppDataBase.kt
+++ ComponentesBasicos/RoomDataBase_kotlin/app/src/main/java/com/joyce/roomdatabase_kotlin/database/AppDataBase.kt
@@ -1,6 +1,38 @@
 package com.joyce.roomdatabase_kotlin.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.joyce.roomdatabase_kotlin.database.daos.UserDao
+import com.joyce.roomdatabase_kotlin.database.models.User
+
 /*  Classe que cria nosso banco de dados  */
 
-class AppDataBase {
+@Database(entities = [User::class], version = 1)
+abstract class AppDataBase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+
+    //garante que tenha apenas uma instancia unica do db na aplicacao  ++
+    companion object {
+
+        private const val DATABASE_NAME: String = "nome_do_banco"
+
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+
+        fun getInstance(context: Context): AppDataBase =
+            INSTANCE ?: kotlin.synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                AppDataBase::class.java, DATABASE_NAME
+            ).build() // ++
+
+    }
 }
